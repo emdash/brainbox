db60fe7f9e02f52091611f7e78473169692fea88
Brandon Lewis <dotsony@gmail.com>
2022-04-19 11:46:09 -0700

    Finishing up OrgToDir
    - added some sanity checking of the import system
    - fixed a bunch of bugs related to that.
    
    Still work to do, but need to fix some performance issues first.

--- orgimport.sh
+++ orgimport.sh
@@ -5,29 +5,49 @@ set -eo pipefail
 
 base="$(pwd)"
 declare -x GTD_DATA_DIR="${base}/org_import/gtdgraph"
-declare -A tags
+TAG_DIR="${base}/org_import/tags"
 
 function gtd         { "${base}/gtd.sh" "$@"; }
+function debug       { echo "$@" >&2 ; }
+function error       { debug "$@" ; exit 1 ; }
 
 function find_or_create_tag {
-    if ! test -v tags["$1"]; then
-	tags["${1}"]="$(gtd graph_node_create)"
+    mkdir -p "${TAG_DIR}"
+    local tag="$1"
+    local id
+    if test -f "${TAG_DIR}/${tag}"; then
+	read -r id < "${TAG_DIR}/${tag}"
+	echo "${id}"
+    else
+	debug "creating tag: ${tag}"
+	id="$(gtd graph_node_create)"
+	echo "${id}" > "${TAG_DIR}/${tag}"
+	echo "PERSIST" | gtd task_state write "${id}"
+	echo "${tag}"  | gtd task_contents write "${id}"
+	gtd task_state exists    "${id}" || error "wtf?"
+	gtd task_contents exists "${id}" || error "wtaf?"
+	echo "${id}"
     fi
-    echo "${tags["${1}"]}"
 }
 
 function process_dir {
+    debug "enter: $1"
     pushd "$1" > /dev/null
     local id="$(gtd graph_node_create)"
     echo "${id}"
+    debug "${id}"
 
     gtd task_state    write "${id}" < state
     gtd task_contents write "${id}" < contents
 
+    gtd task_state exists    "${id}" || error "wtf?"
+    gtd task_contents exists "${id}" || error "wtaf?"
+
     if test -e tags; then
-	for tag in "$(cat tags)"; do
+	while read tag; do
+	    debug "tag: ${tag}" >&2
 	    gtd task_assign "${id}" "$(find_or_create_tag "${tag}")"
-	done
+	done < tags
     fi
 
     ls | while read file; do
@@ -40,16 +60,38 @@ function process_dir {
     popd > /dev/null
 }
 
+function extract_headings {
+    grep '^\*' "$1"
+}
+
 function begin {
+    local path="$1"
     test -e && rm -rf org_import
     mkdir -p org_import
     cd org_import
 
+    # convert org file to directory tree
     echo "creating python dir"
-    ../orgtodir.py
+    ../orgtodir.py < "${path}"
     echo NEW > org_to_dir/state
     echo Root > org_to_dir/contents
 
+    # do some sanity checks on the conversion
+    extract_headings "${path}" > headings
+    if ! diff -u tattle.org headings; then
+	echo "Some headings were not processed. Exiting."
+	exit 1
+    fi
+
+    local -i n_headings="$(wc -l < headings)"
+    local -i n_subdirs="$(find org_to_dir -type d | wc -l)"
+    if test "${n_headings}" -gt "${n_subdirs}"; then
+	echo "Some headings are missing from the output directory. Exiting."
+	echo "headings: ${n_headings}"
+	echo "dirs:     ${n_subdirs}"
+	exit 1
+    fi
+    
     gtd init
     process_dir org_to_dir
 }
--- orgtodir.py
+++ orgtodir.py
@@ -22,147 +22,187 @@ def valid_state(s):
         "REMEMBER"
     }
 
-def next_char():
-    if len(line) and line[0] != '\n':
-        return line[0]
-    else:
-        raise StopIteration
+class Scanner:
 
-def accept():
-    global line
-    line = line[1:]
+    def __init__(self, line):
+        self.line = line
 
-def reject(chars):
-    global line
-    line = chars + line
-
-def skip_whitespace():
-    spaces = ""
-    while next_char().isspace():
-        spaces += next_char()
-        accept()
-    return spaces
-
-def count_stars():
-    count = 0
-    while next_char() == "*":
-        count += 1
-        accept()
-    skip_whitespace()
-    return count
-
-def get_state():
-    state = ""
-    while next_char().isupper():
-        state += next_char()
-        accept()
-
-    if valid_state(state):
-        skip_whitespace()
-        return state
-    else:
-        reject(state)
-        return None
-
-def get_gloss():
-    gloss = ""
-
-    try:
-        while True:
-            if next_char() == ':':
-                possible_tag = next_char()
-                accept()
-                if next_char().isspace():
-                    gloss += possible_tag + next_char()
-                    possible_tag = ""
-                    accept()
-                else:
-                    reject(possible_tag)
-                    return gloss
-            else:
-                gloss += next_char()
-                accept()
-    except StopIteration:
-        # we hit end of input before encountering a ':'
-        return gloss
-
-def get_tags():
-    tags = []
-    tag = ""
-
-    skip_whitespace()
-
-    try:
-        while True:
-            if next_char() == ':':
-                if tag:
-                    tags.append(tag.strip("@"))
-                tag = ""
-                accept()
-            else:
-                tag += next_char()
-                accept()
-    except StopIteration:
-        return tags
-
-def process_line():
-    global line, orig_line
-    try:
-        line = orig_line = sys.stdin.readline()
-        if not line: exit(0)
-    
-        if next_char() == "#":
-            return ("comment", "")
+    def next_char(self):
+        if len(self.line) and self.line[0] != '\n':
+            return self.line[0]
         else:
-            line_depth = count_stars()
-            if line_depth == 0:
-                return ("content", line)
+            raise StopIteration
+    
+    def accept(self):
+        self.line = self.line[1:]
+
+    def accept_line(self):
+        ret = self.line.strip()
+        self.line = ""
+        return ret
+    
+    def reject(self, chars):
+        self.line = chars + self.line
+    
+    def skip_whitespace(self):
+        spaces = ""
+        while self.next_char().isspace():
+            spaces += self.next_char()
+            self.accept()
+        return spaces
+    
+    def count_stars(self):
+        count = 0
+        while self.next_char() == "*":
+            count += 1
+            self.accept()
+        self.skip_whitespace()
+        return count
+    
+    def get_state(self):
+        state = ""
+        while self.next_char().isupper():
+            state += self.next_char()
+            self.accept()
+    
+        if valid_state(state):
+            self.skip_whitespace()
+            return state
+        else:
+            self.reject(state)
+            return "INFO"
+    
+    def get_gloss(self):
+        gloss = ""
+    
+        try:
+            while True:
+                if self.next_char() == ':':
+                    possible_tag = self.next_char()
+                    self.accept()
+                    if self.next_char().isspace():
+                        gloss += possible_tag + self.next_char()
+                        possible_tag = ""
+                        self.accept()
+                    else:
+                        self.reject(possible_tag)
+                        return gloss
+                else:
+                    gloss += self.next_char()
+                    self.accept()
+        except StopIteration:
+            # we hit end of input before encountering a ':'
+            return gloss
+    
+    def get_tags(self):
+        tags = []
+        tag = ""
+        
+        try:
+            self.skip_whitespace()
+            while True:
+                if self.next_char() == ':':
+                    if tag:
+                        tags.append(tag.strip("@"))
+                    tag = ""
+                    self.accept()
+                else:
+                    tag += self.next_char()
+                    self.accept()
+        except StopIteration:
+            return tags
+    
+    def process_line(self):
+        try:
+            if self.next_char() == "#":
+                return ("comment", "")
             else:
-                state = get_state()
-                gloss = get_gloss().strip()
-                tags = get_tags()
-                return ("heading", (line_depth, state, gloss, tags))
-    except StopIteration:
-        return ("content", "")
+                line_depth = self.count_stars()
+                if line_depth == 0:
+                    return ("content", self.accept_line())
+                else:
+                    state = self.get_state()
+                    gloss = self.get_gloss().strip()
+                    tags = self.get_tags()
+                    return ("heading", (line_depth, state, gloss, tags))
+        except StopIteration:
+            return ("content", "")
 
 def process_lines():
-    global line
-    depth = 0
+    path = "."
     stack = []
     ids = gen_ids()
     tattle = open("tattle.org", "w")
+    headings = set([])
 
     os.system("rm -rf org_to_dir")
     os.mkdir("org_to_dir")
     os.chdir("org_to_dir")
 
-    while True:
-        (ty, info) = process_line()
+    for line in sys.stdin:
+        (ty, info) = Scanner(line).process_line()
         print(ty, info)
-        if   ty == "comment": pass
-        elif ty == "content": open("contents", "a").write(info)
+
+        if ty == "comment":
+            pass
+        elif ty == "content":
+            open("contents", "a").write(info)
         elif ty == "heading":
+            tattle.write(line)
+            next_id = ids.__next__()
             line_depth, state, gloss, tags = info
+            # save gloss for sanity checking
+            headings.add(gloss)
 
-            if line_depth > depth:
-                stack.append((depth, os.getcwd()))
-                depth = line_depth
-                next_id = ids.__next__()
-                os.mkdir(str(next_id))
-                os.chdir(str(next_id))
-            else:
-                while line_depth <= depth:
-                    depth, dir = stack.pop()
-                os.chdir(dir)
+            # adjust stack according to line depth this means that the
+            # nesting depth won't necessarily be preserved, but the
+            # structural relationship will.
+            while line_depth < len(stack):
+                stack.pop()
+            stack.append(next_id)
+            path = os.path.join(*(str(p) for p in stack))
+            os.mkdir(path)
 
-            # we are creating a node
-            tattle.write(orig_line)
-            contents = open("contents", "w")
-            contents.write(gloss)
-            open("state","w").write(str(state))
-            open("tags", "w").write(" ".join(tags))
+            assert not os.path.exists(os.path.join(path, "contents"))
+            open(os.path.join(path, "contents"), "w").write(gloss + '\n')
+            open(os.path.join(path, "state"),    "w").write(str(state) + '\n')
+            if tags:
+                open(os.path.join(path, "tags"),     "w").write("\n".join(tags) + '\n')
         else:
             raise ValueError(info)
-        
-line = orig_line = sys.stdin.readline()
+
+    # perform a basic sanity check that we have created a directory
+    # for every heading in the input.
+    actual = actual_headings(os.getcwd(), set([]))
+    if headings != actual:
+        print("Number of missing headings:", len(headings) - len(actual))
+        for line in headings - actual:
+            print(line)
+
+def actual_headings(path, actual):
+    for subpath in (os.path.join(path, sp) for sp in os.listdir(path)):
+        if os.path.isdir(subpath):
+            contents = open(os.path.join(subpath, "contents"), "r").readline()
+            actual.add(contents)
+            actual_headings(os.path.join(path, subpath), actual)
+    return actual
+
+
+assert Scanner("*** TODO Foo bar baz :Bunsen:Rizzo:").process_line() == \
+    ('heading', (3, 'TODO', 'Foo bar baz', ['Bunsen', 'Rizzo']))
+assert Scanner("*** TODO Foo bar baz").process_line()                == \
+    ('heading', (3, 'TODO', 'Foo bar baz', [])) 
+assert Scanner("*** Foo bar baz :Bunsen:Rizzo:").process_line()      == \
+    ('heading', (3, None, 'Foo bar baz', ['Bunsen', 'Rizzo']))
+assert Scanner("*** Foo bar baz").process_line()                     == \
+    ('heading', (3, None, 'Foo bar baz', []))   
+assert Scanner("*** Foo bar baz :Bunsen:").process_line()            == \
+    ('heading', (3, None, 'Foo bar baz', ['Bunsen']))
+assert Scanner(":Bunsen:Rizzo:").get_tags()                          == \
+    ['Bunsen', 'Rizzo']            
+assert Scanner(":Bunsen:").get_tags()                                == \
+    ['Bunsen']                                          
+assert Scanner("").get_tags()                                        == \
+    []
+
 process_lines()
