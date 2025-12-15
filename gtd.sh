c2ad9c13c3776c32e8b12e73f9da524a8ba8f4aa
emdash <dotsony@gmail.com>
2025-12-14 18:18:05 -0800

    Reduce size of scaled chafa image to make room for attitional content.

--- gtd.sh
+++ gtd.sh
@@ -1233,11 +1233,12 @@ function dot {
 query_declare_type             chafa formatter
 query_declare_default_producer chafa from cur subtasks
 function chafa {
+    local -r width="${FZF_PREVIEW_COLUMNS:-"${COLUMNS:-80}"}"
+    local -r height="${FZF_PREVIEW_LINES:-"${LINES:-24}"}"
     end_filter_chain "$@"
     graph dot \
       | env dot -Tpng \
-      | env chafa -s \
-        "${FZF_PREVIEW_COLUMNS:-"${COLUMNS:-80}"}"x"${FZF_PREVIEW_LINES:-"${LINES:-24}"}"
+      | env chafa -s "${width}x$(("${height}" / 2))"
 }
 
 # select nodes from input set to be placed into the given bucket
