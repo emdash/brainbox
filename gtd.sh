9e3b5e71e93ef3bddcbe29228e5c82c113291882
emdash <dotsony@gmail.com>
2026-01-02 10:39:44 -0800

    fix copy-pasta error

--- gtd.sh
+++ gtd.sh
@@ -2562,7 +2562,7 @@ function __interactive_graph_submenu {
     fzf_bind_sexec  "A" "Unassign Source and Target" "$0 unassign"                          "${rls}"
     fzf_bind_sexec  "S" "Swap Source and Target"     "$0 swap source target"                "refresh-preview"
     fzf_bind_sexec  "d" "Link Source and Target"     "$0 add"                               "${rls}"
-    fzf_bind_sexec  "D" "Unlink Source and Target"   "$0 add"                               "${rls}"
+    fzf_bind_sexec  "D" "Unlink Source and Target"   "$0 remove"                            "${rls}"
     fzf_bind_action "b" "Bucket"                     "become($0 __interactive_bucket {+1})" "${rls}"
 }
 
