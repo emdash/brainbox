9418f8094227b03ee5b98fee6d123d02fb75d355
emdash <dotsony@gmail.com>
2026-06-22 15:56:08 -0700

    Start porting scheduler

---
+++ components/Scheduler.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Brainbox.Scheduler where
+
+import Util
+
+main :: IO ()
+main = error "not implemented"
